174f1b54d3edb524c05b1f390a6079e970377028
mazn1chenko <maznichenko.144@gmail.com>
2023-10-12 00:06:45 +0300

    Need to fix bug...
    Cannot fetch data for forecast 7.
    Need to create one more model for forecast 7 days VC.

--- WeatherApp/Model/ForecastWeatherModel.swift
+++ WeatherApp/Model/ForecastWeatherModel.swift
@@ -58,13 +58,14 @@ enum Text: String, Codable {
     case cloudy = "Cloudy"
     case lightDrizzle = "Light drizzle"
     case lightRain = "Light rain"
+    case lightRainShower = "Light rain shower"
+    case mist = "Mist"
     case moderateRain = "Moderate rain"
     case overcast = "Overcast"
     case partlyCloudy = "Partly cloudy"
     case patchyLightDrizzle = "Patchy light drizzle"
     case patchyRainPossible = "Patchy rain possible"
     case sunny = "Sunny"
-    case lightRainShower = "Light rain shower"
 
 
     case мряка = "Мряка"
@@ -135,10 +136,16 @@ struct Hour: Codable {
     let isDay: Int?
     let condition: Condition?
     let windMph, windKph: Decimal?
+    let windDegree: Decimal?
     let pressureMB: Decimal?
+    let pressureIn, precipMm, precipIn: Decimal?
     let humidity, cloud: Decimal?
-    let chanceOfRain: Decimal?
+    let feelslikeC, feelslikeF, windchillC, windchillF: Decimal?
+    let heatindexC, heatindexF, dewpointC, dewpointF: Decimal?
+    let willItRain, chanceOfRain, willItSnow, chanceOfSnow: Decimal?
+    let visKM, visMiles: Decimal?
     let gustMph, gustKph: Decimal?
+    let uv: Decimal?
 
     enum CodingKeys: String, CodingKey {
         case timeEpoch = "time_epoch"
@@ -149,11 +156,29 @@ struct Hour: Codable {
         case condition
         case windMph = "wind_mph"
         case windKph = "wind_kph"
+        case windDegree = "wind_degree"
         case pressureMB = "pressure_mb"
+        case pressureIn = "pressure_in"
+        case precipMm = "precip_mm"
+        case precipIn = "precip_in"
         case humidity, cloud
+        case feelslikeC = "feelslike_c"
+        case feelslikeF = "feelslike_f"
+        case windchillC = "windchill_c"
+        case windchillF = "windchill_f"
+        case heatindexC = "heatindex_c"
+        case heatindexF = "heatindex_f"
+        case dewpointC = "dewpoint_c"
+        case dewpointF = "dewpoint_f"
+        case willItRain = "will_it_rain"
         case chanceOfRain = "chance_of_rain"
+        case willItSnow = "will_it_snow"
+        case chanceOfSnow = "chance_of_snow"
+        case visKM = "vis_km"
+        case visMiles = "vis_miles"
         case gustMph = "gust_mph"
         case gustKph = "gust_kph"
+        case uv
     }
 }
 
